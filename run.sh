312dbc6ec1829ded96bc06217a6c8e9ea02bba5b
Yerong Li <yerong.li@outlook.com>
2021-11-14 23:18:59 -0600

    Reduce number of iterations

--- run.sh
+++ run.sh
@@ -15,10 +15,10 @@ fi
 make
 mkdir -p results
 
-size=50 # embedding dimension
+size=10 # embedding dimension
 negative=5 # number of negative samples
 samples=1 # number of edges (Million) for training at each iteration
-iters=500 # number of iterations
+iters=10 # number of iterations
 threads=20 # number of threads for training
 
 ./bin/esim -model 2 -alpha 0.025 -node ${node_file} -link ${link_file}  -path ${path_file} -output ${output_file} -binary 1 -size ${size} -negative ${negative} -samples ${samples} -iters ${iters} -threads ${threads}
