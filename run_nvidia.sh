309f2176b8e310da050c81903f7f9b9b743781e2
Yerong Li <yerong.li@outlook.com>
2022-04-14 16:15:48 -0500

    Update embedding dimensions

--- run_nvidia.sh
+++ run_nvidia.sh
@@ -5,17 +5,17 @@ link_file="nvidia/link.dat"
 path_file="nvidia/path.dat"
 output_file="nvidia/vec.dat"
 
-if [ ${node_file} == "dblp_data/node.dat" ] && [ ! -e dblp_data/node.dat ]; then
-    echo ${green}===Downloading DBLP Dataset===${reset}
-    curl http://dmserv2.cs.illinois.edu/data/data_dblp.tar.gz --output data_dblp.tar.gz
-    tar -xvf data_dblp.tar.gz
-    mv data_dblp dblp_data
-fi
+# if [ ${node_file} == "dblp_data/node.dat" ] && [ ! -e dblp_data/node.dat ]; then
+#     echo ${green}===Downloading DBLP Dataset===${reset}
+#     curl http://dmserv2.cs.illinois.edu/data/data_dblp.tar.gz --output data_dblp.tar.gz
+#     tar -xvf data_dblp.tar.gz
+#     mv data_dblp dblp_data
+# fi
 
 make
 mkdir -p results
 
-size=10 # embedding dimension
+size=100 # embedding dimension
 negative=5 # number of negative samples
 samples=1 # number of edges (Million) for training at each iteration
 iters=10 # number of iterations
